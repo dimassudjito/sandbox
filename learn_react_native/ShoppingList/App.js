ec9d820379c7990939b03af24311647c7a30db9a
Dimas Sudjito <almuhtad@ualberta.ca>
2022-01-25 14:32:48 -0700

    finish react native course

--- learn_react_native/ShoppingList/App.js
+++ learn_react_native/ShoppingList/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, FlatList, Text, StyleSheet, Alert} from 'react-native';
+
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 123, text: 'Milk'},
+    {id: 124, text: 'Meat'},
+    {id: 125, text: 'Bread'},
+    {id: 126, text: 'Coconut'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert(
+        'No item entered',
+        'Please enter an item when adding to your shopping list',
+        [
+          {
+            text: 'Understood',
+            style: 'cancel',
+          },
+        ],
+        {cancelable: true},
+      );
+    } else {
+      setItems(prevItems => {
+        return [...prevItems, {id: Math.random(), text}];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
@@ -16,11 +56,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {color: 'darkslateblue', fontSize: 30},
-  img: {width: 100, height: 100, borderRadius: 100 / 2},
 });
 
 export default App;
